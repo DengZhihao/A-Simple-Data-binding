fcb4f71ce68f27ace960255a50b64317eed1c8f6
Howard <dzh2013@foxmail.com>
2020-04-07 23:59:04 -0500

    Update Bind.js

--- Bind.js
+++ Bind.js
@@ -1,1 +1,4 @@
+// The difference between call() and bind() is that the call() sets the this keyword and
+// executes the function immediately and it does not create a new copy of the function,
+// while the bind() creates a copy of that function and sets the this keyword.
 
