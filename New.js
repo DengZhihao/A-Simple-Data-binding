698af9be2b0f77502d1581731887f3d25f8da54f
Howard <dzh2013@foxmail.com>
2020-04-12 02:16:17 -0500

    Update New.js

--- New.js
+++ New.js
@@ -1,1 +1,12 @@
-
+function newObj () {
+	var obj = new Object();
+	
+	var constructor = [].shift.call(arguments);
+	
+	obj._proto_ = constructor.prototype;
+	
+	var res = constructor.apply(obj, arguments);
+	
+	return typeof res === 'Object' ? res : obj;
+}
+ 
