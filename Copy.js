bcaeb3f9d580042d59733e87c1ec70f6062099de
Howard <dzh2013@foxmail.com>
2020-04-16 23:43:35 -0500

    Update Copy.js

--- Copy.js
+++ Copy.js
@@ -1,1 +1,21 @@
+var shallowCopy = function(obj){
+	if(typeof obj !== 'object') return;
+	var newObj = obj instanceof Array ? [] : {};
+	for(var i in obj){
+		if(obj.hasOwnPrototype(i)){
+			newObj[i] = obj[i];
+		}
+	}
+	retuen newObj;
+}
 
+var deepCopy = function(){
+	if(typeof obj !== 'object') return;
+	var newObj = obj instanceof Array ? [] : {};
+	for(var i in obj){
+		if(obj.hasOwnPrototype(i)){
+			newObj[i] = typeof newObj[i] === 'object' ? deepCopy(obj[i]) : obj[i];
+		}
+	}
+	retuen newObj;
+}
