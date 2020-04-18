8654ff48c5c8af776d05f9445a695481597f4ab8
Howard <dzh2013@foxmail.com>
2020-04-18 04:36:33 -0500

    Update Debounce&Throttle.js

--- Debounce&Throttle.js
+++ Debounce&Throttle.js
@@ -1,1 +1,38 @@
+function debounce(func,time，immediate){
+	var timeout, result;
+	return function(){
+		var context = this;
+		var args = arguments;
+		
+		if(timeout) clearTimeout(timeout);
+		if(immediate){
+			var now = !timeout;
+			timeout = setTimeout(function(){
+				timeout = null;
+			},time)
+			if(now) result = func.apply(context, args);
+		} else{
+			timeout = setTimeout(function(){
+				func.apply(context, args)
+			},time)
+		}
+		return result;
+	}
+}
 
+function throttle(func,time){
+	var timeout;
+	
+	return function(){
+		var context = this;
+		var args = argumetns;
+		if(!timeout){
+			timeout = setTimeout(
+				function(){
+					timeout = null;
+					func.apply(context, arguments);
+				},time
+			)
+		}
+	}
+}
