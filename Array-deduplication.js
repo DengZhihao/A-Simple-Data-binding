3482fbaddc6167a96e623a3d6b10cb5e87a66596
Howard <dzh2013@foxmail.com>
2020-04-20 23:58:40 -0500

    Update Array-deduplication.js

--- Array-deduplication.js
+++ Array-deduplication.js
@@ -1,1 +1,23 @@
+function deduplication(arr){
+	var res = [];
+	for(var i = 0;i < arr.length;i++){
+		for(var j = 0;j < res.length;j++){
+			if(arr[i] === res[j]){
+				break;
+			}
+		}
+		if(j === res.length){
+		   res.push(arr[i]);
+		}
+	}
+	return res;
+} //Regular
 
+var res = arr.filter((index, item, array) => array.indexOf(item) === index) //ES5
+
+function deduplication(arr){
+	var obj={};
+	var res = arr.filter((index, item, array) => object.hasOwnProperty(typeof item + JSON.stringify(item)) ? false : (obj[typeof item + JSON.stringify(item)] = true)) //Object
+}
+
+var res = (arr) => [...new Set(arr)] //ES6
